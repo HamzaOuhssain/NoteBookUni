b06865589dc7b396cf19bee81c9ffd7d54259c3c
Hamza Ouhssain <hamza.ouhssain.001@student.uni.lu>
2017-11-15 16:19:34 +0100

    Forgot to put all the words of the alphabet

--- app/src/main/java/com/hamza/user/notebook/ShowNoteBook.java
+++ app/src/main/java/com/hamza/user/notebook/ShowNoteBook.java
@@ -34,7 +34,8 @@ public class ShowNoteBook extends AppCompatActivity {
     String [] arrayMenu = {"delete", "modify"};
     ArrayList array_list;
     ArrayList array_listTrad;
-    String[] arrayLetters = new String[]{"%!","A", "B", "C", "D", "E", "F", "G", "E", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z"};
+    String[] arrayLetters = new String[]{"%!","A", "B", "C", "D", "E", "F", "G", "H","I",
+            "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W","X" ,"Y","Z"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
